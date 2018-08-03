d4f5eeef710c5489892fadf4c300ada70d641af7
intern2018 <abelamapip2@gmail.com>
2018-08-03 12:12:51 +0300

    Commit package-lock.json

---
+++ app/Http/Controllers/issuedController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class issuedController extends Controller
+{
+    public function index(){
+        return view('issued');
+    }
+}
