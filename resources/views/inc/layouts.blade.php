ecdae3c30f67c4f6b372df13c515a9104d5fcff4
enguwerk <enguwerk@outlook.com>
2018-08-04 19:40:33 +0100

    frist comit

---
+++ resources/views/inc/layouts.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <script src="{{ asset('/css/app.css') }}" defer></script>
+    <script src="{{ asset('/js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    @yield('content')
+
+</body>
+</html>
