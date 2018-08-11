062baef9d15871999d2a2e3af06d5051c3762adc
kelil harar <kelilherer@gmail.com>
2018-08-11 20:24:19 +0300

    second commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -160,7 +160,7 @@
   <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      Anything you want
+     version 1.0
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2018 <a href="#">K97H</a>.</strong> All rights reserved.
