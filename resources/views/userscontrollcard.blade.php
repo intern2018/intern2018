601b5078f5f7f822893096a6e480f44af69c7cce
kelil harar <kelilherer@gmail.com>
2018-08-11 22:09:58 +0300

    six commit

--- resources/views/userscontrollcard.blade.php
+++ resources/views/userscontrollcard.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  @extends('layouts.master')
  @section('content')
 <div class="container">
@@ -29,80 +28,11 @@
                                 <tbody>
                                 </tbody>
                             </table>
-=======
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>issued</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('css/simple-sidebar.css') }}" rel="stylesheet">
-    
-  </head>
-  <body>
-        <div class="wrapper">
-          @include('includes.sidebar')
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 ml-5 mt-4 pt-4">
-                        <div class="card mt-4">
-                            <div class="card-body text-left">
-                                <h5 class="card-header text-center">table</h5>
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered mt-5">
-                                            <thead>
-                                            <tr>
-                                                <th rowspan="2">ተቁ.</th>
-                                                <th rowspan="2">የእቃው ዝርዝር</th>
-                                                <th rowspan="2">ፒን</th>
-                                                <th rowspan="2">የፋብሪካ ቁጥር</th>
-                                                <th colspan="2">FAIRR ማውጫ ሠነድ</th>
-                                                <th rowspan="2">መለኪያ</th>
-                                                <th rowspan="2">ብዛት</th>
-                                                <th rowspan="2">ጠ/ዋጋ</th>
-                                            </tr>
-                                            <tr>
-                                                <td>No.</td>
-                                                <td>Date</td>
-
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            </tbody>
-                                        </table>
-                                        
-                                    </div>
-                                </div>
-                            </div>
->>>>>>> 2c14e739fb7ae1190eced564acf95a69900aea8d
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
     </div>
 </div>
 @endsection
-=======
-              
-        
-        
-    
-
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-    <script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    </script>
-  </body>
-</html>
->>>>>>> 2c14e739fb7ae1190eced564acf95a69900aea8d
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-@extends('inc.nav')
-
->>>>>>> 2c14e739fb7ae1190eced564acf95a69900aea8d
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
@@ -11,16 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-<<<<<<< HEAD
-=======
-        <script src="{{ asset('/css/app.css') }}" defer></script>
-        <script src="{{ asset('/js/app.js') }}" defer></script>
->>>>>>> 2c14e739fb7ae1190eced564acf95a69900aea8d
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-<<<<<<< HEAD
         <!-- Styles -->
         <style>
             html, body {
@@ -79,13 +68,6 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
             <div  class="container" style="background: #fff">
-=======
-    @section('content')
-    <body>
-       
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
->>>>>>> 2c14e739fb7ae1190eced564acf95a69900aea8d
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -95,7 +77,6 @@
                     @endauth
                 </div>
             @endif
-<<<<<<< HEAD
              </div>
             <div class="content">
                 <div class="title m-b-md">
@@ -103,27 +84,5 @@
                 </div>
             </div>
         </div>
-=======
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a><hr> <br>
-                    <div class="container">
-                     
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        @endsection
->>>>>>> 2c14e739fb7ae1190eced564acf95a69900aea8d
     </body>
 </html>
