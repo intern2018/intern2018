d1c8c19b3cff1ed8df55fe21871b3651fb66f764
intern2018 <abelamapip2@gmail.com>
2018-08-28 10:33:30 +0300

    commit before pull

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,72 +1,55 @@
-<!-- Main Header -->
-<header class="main-header" >
-    <!-- Logo -->
-      <a href="#" class="logo" style="height: 80px;">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><img style="height: 80px; width: 100%;" src="{{ asset('images/logo.png')}}"></span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><img style="height: 80px; width: 100%;" src="{{ asset('images/adm.png')}}"></span>
-      </a>
-    <nav class="navbar navbar-static-top">
-     
-      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-        <span class="sr-only">Toggle navigation</span>
-      </a>
-       <div class="navbar-custom-menu" style="display: inline-block;">
-            <ul class="nav navbar-nav">
-              <li class="dropdown user user-menu">
-                  
-                    
-                <p class="dropdown-toggle" data-toggle="dropdown">
-                  <img height="160px" width="160px" class="user-image" src="{{ asset('images/user4-128x128.jpg')}}" alt="Small">
-                     <span class="hidden-xs">
-                      <small>
-                            Welcome User
-                        </small>
-                     </span>
-                </p>
-                <ul class="dropdown-menu">
-                    <!-- User image -->
-                    <li class="user-header">
+
+  <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+   <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div>
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel" style="background-color: white;">
+          <header class="main-header" id="app">
+            <!-- Logo -->
+            <a href="#" class="logo" style="border-right: 0px;height:100px;width:100%;text-align: left;border-bottom: 1px inset #F9FAFC;">
+              <!-- mini logo for sidebar mini 50x50 pixels -->
+              <span class="logo-mini">Adama Science and Technology University</span>
+              <!-- logo for regular state and mobile devices -->
+              <span class="a"><img height="80px" width="100%" src="{{asset('images/loginLogo.png')}}" alt="Astu icon">
+             </span>
+            </a>
+
+         </header>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+         <footer class="main-footer" style="
+         webkit-transition: -webkit-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+          -moz-transition: -moz-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+          -o-transition: -o-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+          transition: transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+          margin-left: 0px;
+          z-index: 820;border-left: 0px; position: fixed; width: 100%; height: 7%; bottom: 0px; ">
+    <!-- To the right -->
+        <div class="pull-right hidden-xs">
+          version 1.0
+        </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; <script type="text/javascript">document.write(new Date().getFullYear());</script> <a href="#">K97H</a>.</strong> All rights reserved.
   
-  
-                          <img width="150px" height="150px" class="img-circle" src="{{ asset('images/user4-128x128.jpg')}}" alt="Small img 20170303 180515">
-  
-                    </li>
-                    <!-- Menu Body -->
-                    <li class="user-body">
-                      <div class="col-xs-4 text-center">
-  
-                      </div>
-                      <div class="col-xs-4 text-center">
-  
-                      </div>
-                      <div class="col-xs-4 text-center">
-                      </div>
-                    </li>
-                    <!-- Menu Footer-->
-                    <li class="user-footer">
-                      <div class="pull-left">
-                        <a class="btn btn-default btn-flat" href="/auth/edit">Account Settings</a>
-                        <br>
-  
-                      </div>
-                      <div class="pull-right">
-                         <a class="dropdown-item" href="{{ route('logout') }}"
-                             onclick="event.preventDefault();
-                                           document.getElementById('logout-form').submit();">
-                              {{ __('Logout') }}
-                          </a>
-                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                              @csrf
-                          </form>
-                      </div>
-                    </li>
-  
-                  </ul>
-                
-              </li>
-            </ul>
-          </div>
-    </nav>
-  </header>
+      </footer>
+         <!-- Scripts -->
+       <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+</html>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,7 +16,78 @@
 
  <div class="wrapper">
   
-  @include('layouts.header')
+  <!-- Main Header -->
+<header class="main-header" >
+  <!-- Logo -->
+    <a href="#" class="logo" style="height: 80px;">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><img style="height: 80px; width: 100%;" src="{{ asset('images/logo.png')}}"></span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg"><img style="height: 80px; width: 100%;" src="{{ asset('images/adm.png')}}"></span>
+    </a>
+  <nav class="navbar navbar-static-top">
+   
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+      <span class="sr-only">Toggle navigation</span>
+    </a>
+     <div class="navbar-custom-menu" style="display: inline-block;">
+          <ul class="nav navbar-nav">
+            <li class="dropdown user user-menu">
+                
+                  
+              <p class="dropdown-toggle" data-toggle="dropdown">
+                <img height="160px" width="160px" class="user-image" src="{{ asset('images/user4-128x128.jpg')}}" alt="Small">
+                   <span class="hidden-xs">
+                    <small>
+                          Welcome User
+                      </small>
+                   </span>
+              </p>
+              <ul class="dropdown-menu">
+                  <!-- User image -->
+                  <li class="user-header">
+
+
+                        <img width="150px" height="150px" class="img-circle" src="{{ asset('images/user4-128x128.jpg')}}" alt="Small img 20170303 180515">
+
+                  </li>
+                  <!-- Menu Body -->
+                  <li class="user-body">
+                    <div class="col-xs-4 text-center">
+
+                    </div>
+                    <div class="col-xs-4 text-center">
+
+                    </div>
+                    <div class="col-xs-4 text-center">
+                    </div>
+                  </li>
+                  <!-- Menu Footer-->
+                  <li class="user-footer">
+                    <div class="pull-left">
+                      <a class="btn btn-default btn-flat" href="/auth/edit">Account Settings</a>
+                      <br>
+
+                    </div>
+                    <div class="pull-right">
+                       <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                  </li>
+
+                </ul>
+              
+            </li>
+          </ul>
+        </div>
+  </nav>
+</header>
   <!-- Left side column. contains the logo and sidebar -->
   <aside class="main-sidebar">
 
