5cc0b68f26b44c6a91b3a40057aa7e6bf5169322
intern2018 <abelamapip2@gmail.com>
2018-08-28 23:24:09 +0300

    commit before pull

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -94,22 +94,7 @@
     <section class="sidebar mt-5" >
   
       <!-- Sidebar Menu -->
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="treeview menu-open">
-          <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{ url('normaluser/usercard') }}"><i class="fa fa-circle-o"></i>User Card</a></li>
-            <li><a href="{{ url('issued') }}"><i class="fa fa-circle-o"></i>Issued Resource</a></li>
-          </ul>
-        </li>
-        <!-- Optionally, you can add icons to the links -->
-        <li class="active"><a href="{{ url('form') }}"><i class="fa fa-link"></i> <span>Insert Product</span></a></li>
-      </ul>
+      @yield('sidebarmenu')
       <!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
