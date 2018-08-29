53a04e571a43dc8dc32ec8d7df253f101e40e498
intern2018 <abelamapip2@gmail.com>
2018-08-29 08:12:53 +0300

    commit before pull

--- routes/web.php
+++ routes/web.php
@@ -18,45 +18,50 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/issued', 'issuedController@index');
-Route::get('/other-page', function () {
-    return view('welcome');
-});
-Route::get('/form', function () {
-    return view('form');
-});
-Route::get('/test', function () {
-    return view('test');
-});
-/*normaluser*/
-Route::get('normaluser/usercard', function () {
-    return view('normaluser.userscontrollcard');
-});
-Route::get('normaluser/request', function () {
-    return view('normaluser.request');
-});
-Route::get('normaluser/transfer', function () {
-    return view('normaluser.transfer');
-});
-Route::get('/user', function () {
-    return view('normaluser.user');
-});
-/*end normaluser*/
 
-/*store user*/
-Route::get('/suser', function () {
-    return view('storeemp.user');
-});
-Route::get('/issued', function () {
-    return view('storeemp.issued');
-});
-Route::get('/items', 'ItemsController@index');
-Route::get('/registerItem', 'ItemsController@registerItem');
-Route::post('/ItemsController@register','ItemsController@register');
+Route::middleware(['auth'])->group(function(){
 
-Route::get('/registerProviderView', 'ItemsController@registerProviderView');
-Route::post('/ItemsController@registerProvider', 'ItemsController@registerProvider');
-Route::get('/provider', 'ItemsController@provider');
-
-Route::get('/insertItemView', 'ItemsController@insertItemView');
-Route::post('/ItemsController@insertItem', 'ItemsController@insertItem');
+    Route::get('/issued', 'issuedController@index');
+    Route::get('/other-page', function () {
+        return view('welcome');
+    });
+    Route::get('/form', function () {
+        return view('form');
+    });
+    Route::get('/test', function () {
+        return view('test');
+    });
+    /*normaluser*/
+    Route::get('normaluser/usercard', function () {
+        return view('normaluser.userscontrollcard');
+    });
+    Route::get('normaluser/request', function () {
+        return view('normaluser.request');
+    });
+    Route::get('normaluser/transfer', function () {
+        return view('normaluser.transfer');
+    });
+    Route::get('/user', function () {
+        return view('normaluser.user');
+    });
+    /*end normaluser*/
+    
+    /*store user*/
+    Route::get('/suser', function () {
+        return view('storeemp.user');
+    });
+    Route::get('/issued', function () {
+        return view('storeemp.issued');
+    });
+    Route::get('/items', 'ItemsController@index');
+    Route::get('/registerItem', 'ItemsController@registerItem');
+    Route::post('/ItemsController@register','ItemsController@register');
+    
+    Route::get('/registerProviderView', 'ItemsController@registerProviderView');
+    Route::post('/ItemsController@registerProvider', 'ItemsController@registerProvider');
+    Route::get('/provider', 'ItemsController@provider');
+    
+    Route::get('/insertItemView', 'ItemsController@insertItemView');
+    Route::post('/ItemsController@insertItem', 'ItemsController@insertItem');
+    
+});
